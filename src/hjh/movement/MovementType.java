cc41750d0b2b02b22d03636a90b3ff686ebfa6ae
Henri-Laiho <henriharri.laiho@gmail.com>
2019-04-02 11:20:51 +0300

    made abstract

--- src/hjh/movement/MovementType.java
+++ src/hjh/movement/MovementType.java
@@ -5,7 +5,5 @@ import hjh.util.Vector2D;
 import java.awt.geom.*;
 
 public abstract class MovementType {
-    public Point2D evadeBullet(Point2D bulletPos, Vector2D velocity, Point2D myPos, Vector2D myVelocity) {
-        return myPos;
-    }
+    abstract public Point2D evadeBullet(Point2D bulletPos, Vector2D velocity, Point2D myPos, Vector2D myVelocity);
 }
